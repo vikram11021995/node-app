2834585b35d36d85cef1164bf3d06b37d1dfe91a
VIKRAM KUMAR <55698441+vikram11021995@users.noreply.github.com>
2024-05-27 18:02:33 +0530

    Logging intergration - custom & using morgan

--- index.js
+++ index.js
@@ -4,15 +4,36 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/user');
+const fs = require('fs');
+const path = require('path');
+const morgan = require('morgan');
 
 const app = express();
 
 // Middleware
 app.use(bodyParser.json());
-app.use((req,res,next)=>{
-    console.log("API Called : ",req);
-    next()
-})
+
+// Custom middleware to log request details
+// const requestLogger = (req, res, next) => {
+//     const now = new Date();
+//     const logEntry = `[${now.toISOString()}] ${req.method} ${req.url}\n`;
+//     console.log("log : ",logEntry);
+//     fs.appendFile(path.join(__dirname+"/logs", 'access.log'), logEntry, (err) => {
+//         if (err) {
+//           console.error('Failed to write log entry:', err);
+//         }
+//       });
+//     next(); // Call the next middleware function in the stack
+// };
+  
+// Use the custom middleware globally
+// app.use(requestLogger);
+
+//Using morgan library for logging
+// Create a write stream (in append mode)
+const accessLogStream = fs.createWriteStream(path.join(__dirname+"/logs", 'access.log'), { flags: 'a' });
+// Setup morgan to log to the file
+app.use(morgan('combined', { stream: accessLogStream }));
 
 // Routes
 app.use('/api/user', userRoutes);
