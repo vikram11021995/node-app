4493ba79a70f4f76a85074bba502ea81a50ae25a
VIKRAM KUMAR <55698441+vikram11021995@users.noreply.github.com>
2024-05-27 18:05:49 +0530

    Dev (#4)
    * Logging middleware integrated
    
    * fix: changes in workflows ci/cd pipeline
    
    * Logging intergration - custom & using morgan
    
    * Workflow fixes

--- index.js
+++ index.js
@@ -39,6 +39,28 @@ const accessLogStream = fs.createWriteStream(path.join(__dirname+"/logs", 'acces
 // Setup morgan to log to the file
 app.use(morgan('combined', { stream: accessLogStream }));
 
+// Custom middleware to log request details
+// const requestLogger = (req, res, next) => {
+//     const now = new Date();
+//     const logEntry = `[${now.toISOString()}] ${req.method} ${req.url}\n`;
+//     console.log("log : ",logEntry);
+//     fs.appendFile(path.join(__dirname+"/logs", 'access.log'), logEntry, (err) => {
+//         if (err) {
+//           console.error('Failed to write log entry:', err);
+//         }
+//       });
+//     next(); // Call the next middleware function in the stack
+// };
+  
+// Use the custom middleware globally
+// app.use(requestLogger);
+
+//Using morgan library for logging
+// Create a write stream (in append mode)
+const accessLogStream = fs.createWriteStream(path.join(__dirname+"/logs", 'access.log'), { flags: 'a' });
+// Setup morgan to log to the file
+app.use(morgan('combined', { stream: accessLogStream }));
+
 // Routes
 app.use('/api/user', userRoutes);
 // app.use('/api/post', userRoutes);
